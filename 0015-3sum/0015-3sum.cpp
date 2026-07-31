6b875f9db3048a9785f54fd831acfb3502c0affe
Adityairl <139786026+Adityairl@users.noreply.github.com>
2026-08-01 00:37:16 +0530

    Time: 45 ms (71.36%), Space: 29 MB (73.6%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        for(int i = 0;i<nums.size()-2;i++){
+        for(int i = 0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int left = i+1;
-            int right = nums.size()-1;
-            while(left<right){
-                int sum = nums[i]+nums[left]+nums[right];
+            int l = i+1;
+            int r = n-1;
+            while(l<r){
+                int sum = nums[i]+nums[l]+nums[r];
                 if(sum==0){
-                    ans.push_back({nums[i],nums[left],nums[right]});
-                    left++;
-                    right--;
-                    while(left<right && nums[left]==nums[left-1]){
-                    left++;
+                    ans.push_back({nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1]){
+                        l++;
                     }
-                    while(left<right && nums[right]==nums[right+1]){
-                    right--;
+                    while(l<r && nums[r]==nums[r+1]){
+                        r--;
                     }
                 }
                 else if(sum<0){
-                    left++;
+                    l++;
                 }
                 else{
-                    right--;
+                    r--;
                 }
             }
         }

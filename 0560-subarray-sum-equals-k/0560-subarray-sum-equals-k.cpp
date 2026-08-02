5a40012db82d9b523834e7a12ebee0e0bf473d51
Adityairl <139786026+Adityairl@users.noreply.github.com>
2026-08-02 18:03:29 +0530

    Time: 37 ms (78.16%), Space: 46.9 MB (30.14%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int count = 0;
-        vector<int> prefixSum(n);
-        prefixSum[0] = nums[0];
-        for(int i = 1;i<n;i++){
-            prefixSum[i] = prefixSum[i-1] + nums[i];
+        vector<int> prefix(nums.size());
+        prefix[0] = nums[0];
+        for(int i = 1;i<nums.size();i++){
+            prefix[i] = prefix[i-1] + nums[i];
         }
+        int count = 0;
         unordered_map<int,int> mp;
-        for(int j = 0 ;j<n;j++){
-            if(prefixSum[j]==k) count++;
-            int val = prefixSum[j] - k;
+        for(int j = 0;j<nums.size();j++){
+            if(prefix[j]==k) count++;
+            int val  = prefix[j]-k;
             if(mp.find(val)!=mp.end()){
                 count+=mp[val];
             }
-            if(mp.find(prefixSum[j])==mp.end()){
-                mp[prefixSum[j]]=0;
+            if(mp.find(prefix[j])== mp.end()){
+                mp[prefix[j]]=0;
             }
-            mp[prefixSum[j]]++;
+            mp[prefix[j]]++;
         }
         return count;
     }

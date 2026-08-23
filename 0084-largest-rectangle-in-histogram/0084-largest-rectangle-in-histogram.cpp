5f684fe3d34ef1249cd59c5d6addf2f0bb978dae
Adityairl <139786026+Adityairl@users.noreply.github.com>
2026-08-23 15:44:14 +0530

    Time: 58 ms (5.39%), Space: 96.3 MB (5.01%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -4,17 +4,17 @@ public:
         int n = arr.size();
         stack<pair<int,int>> s;
         vector<int> v1;
-        for(int i = 0;i<n;i++){
+        for(int i = 0 ;i<n;i++){
             while(!s.empty() && s.top().first>=arr[i]){
                 s.pop();
-            }           
+            }
             if(s.empty()){
                 v1.push_back(-1);
             }
             else{
                 v1.push_back(s.top().second);
             }
-            s.push({arr[i],i});                    
+            s.push({arr[i],i});
         }
         return v1;
     }
@@ -25,17 +25,18 @@ public:
         for(int i = n-1;i>=0;i--){
             while(!s.empty() && s.top().first>=arr[i]){
                 s.pop();
-            }           
+            }
             if(s.empty()){
                 v2.push_back(n);
             }
             else{
                 v2.push_back(s.top().second);
             }
-            s.push({arr[i],i});                    
+            s.push({arr[i],i});
         }
         reverse(v2.begin(),v2.end());
         return v2;
+       
     }
     int largestRectangleArea(vector<int>& arr) {
         int n = arr.size();
@@ -43,9 +44,9 @@ public:
         vector<int> v2 = NSR(arr);
         int ans = 0;
         for(int i = 0;i<n;i++){
-            int w = v2[i]-v1[i]-1;
-            int area = arr[i] * w;
-            ans = max(ans,area);
+            int width = v2[i]-v1[i]-1;
+            int area = arr[i]*width;
+            ans = max(area,ans);
         }
         return ans;
     }
